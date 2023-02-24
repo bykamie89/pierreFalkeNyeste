8936255ce661ae799c19aad5206a22637752a320
Kamilla <kamillakkngmail.com>
2023-02-24 09:57:46 +0100

    cganged links images

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,7 @@ module.exports = {
       fontSize: {
         h1SizeMobile: '48.83px',
         h1SizeWeb: '120px',
+        h1SizeWebForside: '108px',
         h2SizeMobile: '31.25px',
         h2SizeWeb: '36px',
         h2SizeWebQuote: '55px',
